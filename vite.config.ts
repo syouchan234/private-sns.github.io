4363ac10bafd04914d7ab98d5dc75fde904f5254
Syouchan <syou5963@outlook.jp>
2026-04-12 10:21:40 +0900

    add commmit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/private-sns.github.io/',// リポジトリ名を指定
   plugins: [
     react(),
     legacy()
